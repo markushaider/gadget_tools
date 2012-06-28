f7bd00245b119d58ca0736dc0b90bd1f18bc6a37
harre <harald.hoeller@gmx.net>
2012-06-28 16:30:02 +0200

    cutout update

--- cutout.c
+++ cutout.c
@@ -13,18 +13,18 @@ int main(int argc, char** argv) {
   infile=argv[1];
   char *outfile;
   outfile=argv[2];
-  float clumpx, clumpy, clumpz; 
-  clumpx=atoi(argv[3]);
-  clumpy=atoi(argv[4]);
-  clumpz=atoi(argv[5]);
 
+  float clumpx, clumpy, clumpz;
+  sscanf (argv[3],"%f",&clumpx);
+  sscanf (argv[4],"%f",&clumpy);
+  sscanf (argv[5],"%f",&clumpz);
+    
   if(argc!=6) {
-    printf("Usage: ./cutout <infile> <outfile> <x coordinate of clump> <y coordinate> <z coordinate>\n");
+    printf("Usage: ./cutout <infile> <outfile> <x coordinate of clump [Mpc]> <y coordinate> <z coordinate>\n");
     return 0;
   }
 
-  printf("Box centered around x: %g y: %g z: %g \n", clumpx, clumpy, clumpz);
-
+  printf("Box centered around x: %g y: %g z: %g [Mpc] \n", clumpx, clumpy, clumpz);
 
   /* load snapshot file */
   io_header header;
@@ -32,9 +32,7 @@ int main(int argc, char** argv) {
   int nTot;
   nTot = load_snapshot(infile, &header, &P);
 
-    printf("2 Box centered around x: %g y: %g z: %g \n", clumpx, clumpy, clumpz);
-
-  
+ 
   /* output information about the simulation */
   printf(">> Output from Header \n");
   printf("Boxsize of Simulation: %g\n", header.BoxSize);
@@ -45,21 +43,25 @@ int main(int argc, char** argv) {
 
   /*cutout region around the center */
   float extent=20000;
-  
-    printf("3 Box centered around x: %g y: %g z: %g \n", clumpx, clumpy, clumpz);
+  clumpx=clumpx*1000; 		// Mpc -> kpc 
+  clumpy=clumpy*1000; 		// Mpc -> kpc 
+  clumpz=clumpz*1000; 		// Mpc -> kpc 
 
-  
+ 
   /* check if the cutout region does not extend over boundaries */ 
-  if(header.BoxSize-extent>clumpx || extent>clumpx) {
+  if(header.BoxSize-extent/2.<clumpx || extent/2.>clumpx) {
     printf("Out of bound in x-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
     return 0;  
   }
-  if(header.BoxSize-extent>clumpy || extent>clumpy) {
+  if(header.BoxSize-extent/2.<clumpy || extent/2.>clumpy) {
     printf("Out of bound in y-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
     return 0;  
   }
-  if(header.BoxSize-extent>clumpz || extent>clumpz) {
+  if(header.BoxSize-extent/2.<clumpz || extent/2.>clumpz) {
     printf("Out of bound in z-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
     return 0;  
   } 
 
