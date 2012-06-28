6df9caaac7987a50783115936d63afda78404ec1
Markus Haider <markus.haider@uibk.ac.at>
2012-06-28 15:35:26 +0200

    added modifications of harald

--- cutout.c
+++ cutout.c
@@ -13,10 +13,18 @@ int main(int argc, char** argv) {
   infile=argv[1];
   char *outfile;
   outfile=argv[2];
-  if(argc!=3) {
-    printf("Usage: ./cutout <infile> <outfile>\n");
+
+  float clumpx, clumpy, clumpz;
+  sscanf (argv[3],"%f",&clumpx);
+  sscanf (argv[4],"%f",&clumpy);
+  sscanf (argv[5],"%f",&clumpz);
+    
+  if(argc!=6) {
+    printf("Usage: ./cutout <infile> <outfile> <x coordinate of clump [Mpc]> <y coordinate> <z coordinate>\n");
     return 0;
   }
+
+  printf("Box centered around x: %g y: %g z: %g [Mpc] \n", clumpx, clumpy, clumpz);
 
   /* load snapshot file */
   io_header header;
@@ -24,8 +32,10 @@ int main(int argc, char** argv) {
   int nTot;
   nTot = load_snapshot(infile, &header, &P);
 
+ 
   /* output information about the simulation */
-  printf("Boxsize: %g\n", header.BoxSize);
+  printf(">> Output from Header \n");
+  printf("Boxsize of Simulation: %g\n", header.BoxSize);
   printf("Number of gas particles: %i\n", header.npart[0]);
   printf("Number of high-res dm particles: %i\n", header.npart[1]);
   printf("Number of low-res dm particles: %i\n", header.npart[5]);
@@ -33,24 +43,47 @@ int main(int argc, char** argv) {
 
   /*cutout region around the center */
   float extent=20000;
-  
+  clumpx=clumpx*1000; 		// Mpc -> kpc 
+  clumpy=clumpy*1000; 		// Mpc -> kpc 
+  clumpz=clumpz*1000; 		// Mpc -> kpc 
+
+ 
+  /* check if the cutout region does not extend over boundaries */ 
+  if(header.BoxSize-extent<clumpx || extent>clumpx) {
+    printf("Out of bound in x-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
+    return 0;  
+  }
+  if(header.BoxSize-extent<clumpy || extent>clumpy) {
+    printf("Out of bound in y-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
+    return 0;  
+  }
+  if(header.BoxSize-extent<clumpz || extent>clumpz) {
+    printf("Out of bound in z-dimension - chose a more centered clump \n" );
+    printf(">> ABORTING \n" );
+    return 0;  
+  } 
+
+    
   int i;
   /* loop over all particles */
   int counter = 0;
   float center = header.BoxSize/2;
   for (i=0;i<nTot;i++) {
     /* test if particle is within our desired volume */
-    if(P[i].Pos[0]<center+extent/2 && P[i].Pos[0]>center-extent/2 &&
-       P[i].Pos[1]<center+extent/2 && P[i].Pos[1]>center-extent/2 &&
-       P[i].Pos[2]<center+extent/2 && P[i].Pos[2]>center-extent/2) {
+    if(P[i].Pos[0]<clumpx+extent/2 && P[i].Pos[0]>clumpx-extent/2 &&
+       P[i].Pos[1]<clumpy+extent/2 && P[i].Pos[1]>clumpy-extent/2 &&
+       P[i].Pos[2]<clumpz+extent/2 && P[i].Pos[2]>clumpz-extent/2) {
       /* shift coordinates */
-      P[i].Pos[0]-= center-extent/2;
-      P[i].Pos[1]-= center-extent/2;
-      P[i].Pos[2]-= center-extent/2;
+      P[i].Pos[0]-= clumpx-extent/2;
+      P[i].Pos[1]-= clumpy-extent/2;
+      P[i].Pos[2]-= clumpz-extent/2;
       P[counter] = P[i];
       
       counter++;
     }
+
   }
   int counterType [6] = {0,0,0,0,0,0};
   /* get the numbers of the particle types */
@@ -86,12 +119,13 @@ int main(int argc, char** argv) {
   header.BoxSize = extent;
 
   /* output information about the simulation */
-  printf("Output Cutout Box\n");
+  printf(">> Output Cutout Box\n");
   printf("Boxsize: %g\n", header.BoxSize);
   printf("Number of gas particles: %i\n", header.npart[0]);
   printf("Number of high-res dm particles: %i\n", header.npart[1]);
   printf("Number of low-res dm particles: %i\n", header.npart[5]);
   printf("Number of star particles: %i\n", header.npart[4]);
+  printf("Box centered around x: %g y: %g z: %g \n", clumpx, clumpy, clumpz);
 
 
   /* write reduced snapshot file */
